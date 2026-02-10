099df134e27b04dea6d08684b9bda4d8a5d946da
Martin Tvrdoň <martin.tvrdon@student.tuke.sk>
2026-02-10 21:02:08 +0100

    hehi

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://147.232.205.193:5001";
+declare const __API_BASE_URL__: string;
+export const BASE_URL = __API_BASE_URL__;
 
 export interface ApiOptions extends RequestInit {
     json?: any;          // shortcut pre body
