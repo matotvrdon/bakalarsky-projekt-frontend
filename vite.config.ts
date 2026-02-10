8a1fdcf8fe9563512a923560cba381f1fa0b50f7
Martin Tvrdoň <martin.tvrdon@student.tuke.sk>
2026-02-10 21:09:53 +0100

    hehi

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    __API_BASE_URL__: JSON.stringify(process.env.VITE_API_BASE_URL || "http://localhost:5010"),
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    define: {
+      __API_BASE_URL__: JSON.stringify(env.VITE_API_BASE_URL || "http://localhost:5010"),
+    },
+  }
 })
